ef560d592f2e43c057839d5d5d53d023b591d619
Ottodebac <nicolasbrtn7@gmail.com>
2023-04-28 09:30:26 -0700

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const aboutLink = document.getElementById("show-about-btn");
+  const aboutLink = document.getElementById("about-btn");
   const contactLink = document.getElementById("show-contact-btn");
   const workBtn = document.getElementById("work-btn");
   const aboutSection = document.getElementById("about-section");
@@ -14,11 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
   // Add event listeners to the links
   aboutLink.addEventListener("click", function (event) {
     event.preventDefault();
-    console.log(test);
+    console.log("test");
     aboutSection.style.display = "block";
     contactSection.style.display = "none";
     projectSection.style.display = "none";
     heroSection.style.display = "none";
+    activateLink("about-btn");
   });
 
   contactLink.addEventListener("click", function (event) {
@@ -27,6 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
     aboutSection.style.display = "none";
     projectSection.style.display = "none";
     heroSection.style.display = "none";
+    activateLink("show-contact-btn");
   });
 
   workBtn.addEventListener("click", function (event) {
@@ -35,5 +37,15 @@ document.addEventListener("DOMContentLoaded", function () {
     contactSection.style.display = "none";
     projectSection.style.display = "block";
     heroSection.style.display = "block";
+    activateLink("work-btn");
   });
+
+  function activateLink(linkId) {
+    const activeLink = document.querySelector(".navbar a.active");
+    if (activeLink) {
+      activeLink.classList.remove("active");
+    }
+    const link = document.getElementById(linkId);
+    link.classList.add("active");
+  }
 });
